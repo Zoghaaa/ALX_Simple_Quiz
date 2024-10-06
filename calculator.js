b0c9d34e829304f625a21e695836579221c5b3e0
zogha <fz.bendehbi@gmail.com>
2024-10-06 21:16:52 +0100

    add changes

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ function add(number1, number2){
     return number1 + number2;
 }
 
-function substract(number1, number2){
+function subtract(number1, number2){
     return number1 - number2;
 }
 
@@ -21,10 +21,10 @@ document.getElementById('add').addEventListener('click', function() {
     document.getElementById('calculation-result').textContent = result;
 });
 
-document.getElementById('substract').addEventListener('click', function() {
+document.getElementById('subtract').addEventListener('click', function() {
     const number1 = parseFloat(document.getElementById('number1').value) || 0;
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
-    const result = substract(number1, number2);
+    const result = subtract(number1, number2);
     document.getElementById('calculation-result').textContent = result;
 });
 
